3301725caa1d9bc8c49f8c097aea9d1e48f59c4b
Nandha.S <nandhakumar.s@molecularconnections.com>
2025-08-01 18:28:49 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Configuration for Cloudflare Pages
+  output: 'export',
   trailingSlash: false,
   skipTrailingSlashRedirect: false,
   images: {
     unoptimized: true
-  }
+  },
+  distDir: 'out'
 };
 
 export default nextConfig;
